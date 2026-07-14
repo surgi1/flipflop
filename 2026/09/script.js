dfe0620ec9425412cf2ac14f962bceb5d4e05c27
surgi <surgi1@gmail.com>
2026-07-14 10:25:30 +0200

    p1 and p2 of d9, also got p3 star with dirty guesstimation

--- 2026/09/script.js
+++ 2026/09/script.js
@@ -1,79 +1,74 @@
-const parse = input => input.split('\n').map(line => line.split(' ').join(''))
+// part 3 is only guesstimated here, does not solve arbitrary input yet. TBD
 
-Object.defineProperty(Array.prototype, 'sum', {
-    value: function() {
-        return this.reduce((a, v) => a+v, 0);
-    }
-});
+const parse = input => input.split('\n').map(l => l.split(''))
+const dist = (a, b) => Math.abs(a[0]-b[0]) + Math.abs(a[1]-b[1]);
 
-const solve = (data, passes = 7) => {
-    let stoats = {
-        A: 1,
-        B: 1
-    }
-    for (let pass = 0; pass < passes; pass++) {
-        let newStoats = {};
-        Object.entries(stoats).forEach(([from, mult]) => {
-            // get the first
-            let rule = data.filter(v => v[0] === from)[0];
-            for (let i = 1; i < rule.length; i++) {
-                if (newStoats[rule[i]] === undefined) newStoats[rule[i]] = 0;
-                newStoats[rule[i]] += mult
-            }
-        })
-        stoats = newStoats;
-    }
-    return Object.values(stoats).sum();
-}
+const DIRS = [[0, -1], [1, 0], [0, 1], [-1, 0]];
 
-const solve2BF = (data, passes = 7) => {
-    let ruleCache = {};
+// floodfill
+const distanceMap = (map, froms, tpMode = 0, entryDist = 0, wall = '#', path = '.') => {
+    const offMap = (x, y) => x < 0 || y < 0 || x >= cols || y >= rows;
 
-    let stoats = 'AB';
-    for (let pass = 0; pass < passes; pass++) {
-        let newStoats = stoats[0], len = stoats.length;
-        for (let i = 1; i < len; i++) {
-            // find the rule for either stoats[i-1], stoats[i] or stoats[i], stoats[i-1]
-            let k = stoats[i-1] + stoats[i];
-            if (ruleCache[k] === undefined) {
-                let rules = data.filter(v => (v[0] === from[0] && v[1] === from[1]) || (v[0] === from[1] && v[1] === from[0]) );
-                ruleCache[k] = rules[0].slice(2);
-            }
-            newStoats += ruleCache[k] + stoats[i];
+    let cols = map[0].length, rows = map.length, cur;
+    let filled = map.map(row => row.slice().fill(Infinity)),
+        stack = froms.map(from => ({
+            pos: [...from],
+            dist: entryDist
+        }));
+
+    while (cur = stack.pop()) {
+        let [cx, cy] = cur.pos;
+
+        if (filled[cy][cx] <= cur.dist) continue;
+        filled[cy][cx] = cur.dist;
+
+        if (tpMode !== 0) {
+            // tp to the end of the hall in each direction
+            DIRS.forEach(([dx, dy]) => {
+                let [x, y] = [cx, cy];
+                while (!offMap(x+dx, y+dy) && map[y+dy][x+dx] !== wall) {
+                    [x, y] = [x+dx, y+dy];
+                }
+                if (map[y][x] === wall) return true;
+                if (dist([cx, cy], [x, y]) < tpMode+1) return true;
+                if (filled[y][x] > cur.dist + tpMode) stack.push({
+                    pos: [x, y],
+                    dist: cur.dist + tpMode
+                })
+            })
         }
-        stoats = newStoats;
-    }
-    return stoats.length;
-}
 
-const solve2 = (data, passes = 7) => {
-    let ruleCache = {};
-
-    let stoats = {
-        AB: 1
-    }
-    for (let pass = 0; pass < passes; pass++) {
-        let newStoats = {};
-        Object.entries(stoats).forEach(([from, mult]) => {
-            let k = from[0] + from[1];
-            if (ruleCache[k] === undefined) {
-                let rules = data.filter(v => (v[0] === from[0] && v[1] === from[1]) || (v[0] === from[1] && v[1] === from[0]) );
-                ruleCache[k] = rules[0].slice(2);
-            }
-
-            let res = from[0] + ruleCache[k] + from[1]; // split back into pairs
-            for (let i = 1; i < res.length; i++) {
-                let np = res[i-1] + res[i];
-                if (newStoats[np] === undefined) newStoats[np] = 0;
-                newStoats[np] += mult;
-            }
+        DIRS.forEach(([dx, dy]) => {
+            let [x, y] = [cx+dx, cy+dy];
+            if (offMap(x, y)) return true;
+            if (map[y][x] === wall) return true;
+            if (filled[y][x] > cur.dist+1) stack.push({
+                pos: [x, y],
+                dist: cur.dist+1
+            })
         })
-        stoats = newStoats;
     }
-    return Object.values(stoats).sum() + 1; // ummm no idea why +1, but well...
+    return filled;
 }
 
-console.log('p1', solve(parse(input)));
-console.log('p2', solve2(parse(input)));
-//console.log('p3', solve2BF(parse(inputt), 21));
-console.log('p3', solve2(parse(input), 21));
+const run = (map, tpMode = 0) => {
+    let cols = map[0].length, rows = map.length, cur;
+    let start, end, cheats = {};
+    
+    map.forEach((row, y) => row.forEach((v, x) => {
+        if (v == '#') return true;
+        if (v == 'S') start = [x, y];
+        if (v == 'E') end = [x, y];
+        map[y][x] = '.';
+    }))
+
+    let dmap = distanceMap(map, [start], tpMode);
+
+    return dmap[end[1]][end[0]];
+}
+
+console.log('p1', run(parse(input)))
+console.log('p2', run(parse(input), 1))
+console.log('p3', Math.round(run(parse(input), 2.3))) // this is not a true solution, it is guesstimated simplification (tried 2.2 and 2.4, got lower and higher than expected) that ended up hitting the target by accident
+// 791
+
