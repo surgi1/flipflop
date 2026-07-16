9553a2ad44379dfe7e47c154723e98c63b31b300
surgi <surgi1@gmail.com>
2026-07-16 09:38:07 +0200

    p1 of d11

---
+++ 2026/11/script.js
@@ -0,0 +1,177 @@
+Object.defineProperty(Array.prototype, 'chunk', {
+    value: function(chunkSize) {
+        let res = [];
+        for (let i = 0; i < this.length; i += chunkSize) res.push(this.slice(i, i + chunkSize));
+        return res;
+    }
+});
+
+const MAX_AGE = 100;
+
+const D = {
+    DOWN: 0,
+    RIGHT: 1,
+    UP: 2,
+    LEFT: 3
+}
+
+const DIRS = [[0, -1], [1, 0], [0, 1], [-1, 0]];
+
+const P = {
+    SPROUT: 0,
+    STEM: 1,
+}
+
+const STATE = {
+    ALIVE: 0,
+    DEAD: 1,
+}
+
+const parseTreeRules = lit => {
+    let [top, bottom] = lit.split('\n').map(l => l.match(/\S+/g))
+    return top.map((topId, i) => {
+        return {
+            topId: topId,
+            leftId: bottom[i*3],
+            fromId: bottom[i*3+1],
+            rightId: bottom[i*3+2],
+        }
+    })
+}
+
+const parse = input => input.split('\n\n').map(lit => ({
+    rules: parseTreeRules(lit),
+    parts: [{
+        type: P.SPROUT,
+        id: '00',
+        x: 0,
+        y: 1, // effective height
+    }], // init
+    age: 0,
+    state: STATE.ALIVE,
+}))
+
+// Important: What happens if 2 sprouts of the same tree grow into the same location?
+// The resulting sprout with the highest DNA id will take priority and grow into that location
+// (not the sprouts it came from, it's about the sprout ids that try to appear there).
+
+const grow = tree => {
+    if (tree.state !== STATE.ALIVE) return;
+
+    const growSprout = (p, newId, dir) => {
+        if (newId === 'XX') return;
+        // check for existing stems, do not grow into stem
+        let x = p.x + DIRS[dir][0],
+            y = p.y + DIRS[dir][1];
+
+        let blockages = tree.parts
+                .filter(p => p.type === P.STEM)
+                .filter(p => p.x === x && p.y === y);
+
+        if (blockages.length > 0) {
+            //console.log('blockages for dir', DIRS[dir], blockages);
+            return;
+        }
+
+        tree.parts.push({ id: newId, type: P.SPROUT, x: x, y: y })
+    }
+
+    // grow all thesprouts at once
+    // then sanitize the tree, no 2 sprouts can grow into same spot
+    let sprouts = tree.parts.filter(p => p.type === P.SPROUT);
+    sprouts.forEach(p => {
+        // find the rule for this id
+        let rule = tree.rules.find(r => r.fromId === p.id);
+        if (!rule) {
+            console.log('couldnt find a rule for id', id, tree);
+            return true;
+        }
+        //console.log('growing', p, rule)
+        // insert new sprouts
+        growSprout(p, rule.topId, D.UP);
+        growSprout(p, rule.leftId, D.LEFT);
+        growSprout(p, rule.rightId, D.RIGHT);
+        p.turnIntoStem = true;
+    })
+    
+    tree.parts.filter(p => p.turnIntoStem === true).forEach(p => {
+        p.type = P.STEM;
+        delete p.turnIntoStem;
+    });
+
+    // sanitize: no stems on the same spot
+    let stems = tree.parts.filter(p => p.type === P.STEM);
+    /*let sanitizedStems = [];
+
+    stems.forEach(p => {
+        let stemsAtSpot = sanitizedStems.findIndex(s => s.x === p.x && s.y === p.y);
+        if (stemsAtSpot === -1) {
+            sanitizedStems.push(p);
+        } else {
+            if (Number(sanitizedStems[stemsAtSpot].id) < Number(p.id)) sanitizedStems[stemsAtSpot].id = p.id;
+        }
+    })*/
+
+    // sanitize: no sprouts at the stem spots
+    sprouts = tree.parts.filter(p => p.type === P.SPROUT).filter(p => {
+        let stemsAtSpot = stems.filter(stem => stem.x === p.x && stem.y === p.y);
+        return stemsAtSpot.length === 0;
+    })
+    let sanitizedSprouts = [];
+    sprouts.forEach(p => {
+        let sproutAtSpot = sanitizedSprouts.findIndex(s => s.x === p.x && s.y === p.y);
+        if (sproutAtSpot === -1) {
+            sanitizedSprouts.push(p);
+        } else {
+            if (Number(sanitizedSprouts[sproutAtSpot].id) < Number(p.id)) sanitizedSprouts[sproutAtSpot].id = p.id;
+        }
+    })
+
+    tree.parts = [...stems, ...sanitizedSprouts];
+
+    tree.age++;
+
+    if (tree.age >= MAX_AGE) tree.state = STATE.DEAD;
+
+    // energy check
+    if (tree.age >= 5) {
+        let required = tree.parts.length * 3;
+        stems = tree.parts.filter(p => p.type === P.STEM);
+        let produced = stems.reduce((total, stem, i) => {
+            let base = Math.min(10, stem.y);
+            let stemsAbove = stems.filter(s => s.x === stem.x && s.y > stem.y).length;
+            let mult = Math.max(0, 3 - stemsAbove);
+            return total + base*mult;
+        }, 0);
+
+        //console.log('energy check at age', tree.age, 'required:', required, 'produced:', produced, 'mass:', tree.parts.length, tree);
+        if (required > produced) tree.state = STATE.DEAD;
+    }
+
+    //console.log('tree aged');
+}
+
+let el = document.getElementById('root');
+let zoom = 11;
+
+const draw = (tree) => {
+    let s = '';
+    tree.parts.forEach(p => {
+        let x = (p.x + 70)*zoom;
+        let y = (70 - p.y)*zoom;
+        s += '<div class="cell ' + (p.type === P.SPROUT ? 'sprout' : '') + '" style="top:' + y + 'px;left:' + x + 'px">&nbsp;</div>' + '\n';
+    })
+    el.innerHTML = s;
+}
+
+const run = (trees) => {
+    while (trees.filter(t => t.state === STATE.ALIVE).length > 0) {
+        trees.forEach(t => grow(t));
+    }
+
+    //draw(trees[0]);
+    console.log(trees);
+    return trees.reduce((a, t) => a + t.parts.length, 0);
+}
+
+console.log('p1', run(parse(input)))
