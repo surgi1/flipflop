6b42bce0178b6aa6b5db1a5daa7fc68f53e483b3
surgi <surgi1@gmail.com>
2026-07-18 14:03:10 +0200

    rewritten into proper any-dimension solution for d12

--- 2026/12/script.js
+++ 2026/12/script.js
@@ -1,11 +1,4 @@
-// for p2 see script.p1.js
-// for proper p3 see script.p3.js
-
-Object.defineProperty(Array.prototype, 'sum', {
-    value: function() {
-        return this.reduce((a, v) => a+v, 0);
-    }
-});
+// proper any-dimension solution
 
 Object.defineProperty(Array.prototype, 'chunk', {
     value: function(chunkSize) {
@@ -15,102 +8,122 @@ Object.defineProperty(Array.prototype, 'chunk', {
     }
 });
 
-const countBingos2d = (card, div = 2) => {
-    let res = 0;
+const parse = input => input.split('\n\n').map(section => section.split('\n').join(' ').split(' ').map(Number))
+const vectInside = (vect) => vect.every((v, i) => i === 0 || (v >= 0 && v < 5));
+const addVect = (a, b) => a.map((v, i) => v + b[i]);
+const mulVect = (a, n) => a.map((v, i) => n * v);
 
-    // rows
-    card.map(row => {
-        if (row.sum() === 0) res++;
-    })
-
-    // cols
-    card[0].map((_, x) => {
-        if (card.map(row => row[x]).sum() === 0) res++;
-    })
-
-    res = res / div;
-
-    let d1 = 0, d2 = 0;
-    for (let i = 0; i < 5; i++) {
-        d1 += card[i][i];
-        d2 += card[4-i][i];
-    }
-    if (d1 === 0) res++;
-    if (d2 === 0) res++;
-    return res;
-}
-
-const cubeIntoSlices = card => {
-    let slices = [];
-
-    // xform into slices (2d cards) by each axis
-    for (let i = 0; i < 5; i++) {
-        let slice1 = [];
-        let slice2 = [];
-        let slice3 = [];
-        for (let j = 0; j < 5; j++) {
-            slice1[j] = [];
-            slice2[j] = [];
-            slice3[j] = [];
-            for (let k = 0; k < 5; k++) {
-                slice1[j][k] = card[i][j][k];
-                slice2[j][k] = card[j][k][i];
-                slice3[j][k] = card[k][i][j];
-            }
-        }
-        slices.push(slice1, slice2, slice3);
-    }
-
-    return slices;
-}
-
-const countBingos3d = (card, div) => {
-    let res = 0;
-
-    let slices = cubeIntoSlices(card);
-    res = Math.round(slices.map(slice => countBingos2d(slice, div)).sum()); // the twist: each non-diagonal line is processed counted twice for 3d cube
-
-    // 3d diagonals are left now
-    let d1 = 0, d2 = 0, d3 = 0, d4 = 0;
-    for (let i = 0; i < 5; i++) {
-        d1 += card[i][i][i];
-        d2 += card[4-i][i][i];
-        d3 += card[i][4-i][i];
-        d4 += card[4-i][4-i][i];
-    }
-    if (d1 === 0) res++;
-    if (d2 === 0) res++;
-    if (d3 === 0) res++;
-    if (d4 === 0) res++;
-    return res;
-}
-
-const parse = input => {
-    let [calledLit, cardsLit] = input.split('\n\n');
-    return [calledLit.split('\n').join(' ').split(' ').map(Number), cardsLit.split('\n').join(' ').split(' ').map(Number).chunk(5*5*5).map(c => c.chunk(5*5).map(h => h.chunk(5) ) )]
-}
-
-const run = ([hits, cards], div = 2) => {
+const run = ([hits, cards1d], dim = 2) => {
     const turn = nr => cards = cards.map(card => {
         card = card.map(slice => slice.map( row => row.map(v => v == nr ? 0 : v) ) )
         return card;
     })
 
-    let res = false;
-    for (let h = 0; h < hits.length; h++) {
-        let nr = hits[h];
-        turn(nr);
+    // next I need generic-dimensional walk thru the cards array while keeping used coords that will later serve as pointers in the multi-dimensional card(s) array
+    // purpose is to get coords of just marked number
+    const getHitCoords = (nr) => {
+        let hitCoords = false;
 
-        let bingos = cards.map(card => countBingos3d(card, div)).sum();
-        if (bingos >= 5) {
+        const recur = (coords, tmp) => {
+            if (hitCoords !== false) return;
+            if (Array.isArray(tmp)) {
+                tmp.forEach((v, i) => {
+                    recur([...coords, i], v);
+                })
+            } else {
+                if (tmp === nr) hitCoords = coords;
+            }
+        }
+
+        recur([], cards);
+
+        return hitCoords;
+    }
+
+    const setVal = (coords, val, coordId = 0, tmp = cards) => {
+        //if (Array.isArray(tmp[ coords[coordId] ])) {
+        if (coordId < coords.length-1) {
+            setVal(coords, val, coordId+1, tmp[ coords[coordId] ]);
+        } else {
+            tmp[ coords[coordId] ] = val;
+        }
+    }
+
+    const getVal = (coords, coordId = 0, tmp = cards) => {
+        //if (Array.isArray(tmp[ coords[coordId] ])) {
+        if (coordId < coords.length-1) {
+            return getVal(coords, coordId+1, tmp[ coords[coordId] ]);
+        } else {
+            return tmp[ coords[coordId] ];
+        }
+    }
+
+    let cards = cards1d.slice(0);
+    for (let d = 0; d < dim; d++) cards = cards.chunk(5);
+    //console.log(cards);
+
+    // construct valid directional vectors
+    let DIRS = [];
+
+    const createDirRecur = (tmp, dim) => {
+        //console.log('createDirRecur called', tmp, dim)
+        if (dim === 0) return DIRS.push(tmp);
+        [-1, 0, 1].forEach(v => createDirRecur([...tmp, v], dim-1));
+    }
+
+    createDirRecur([], dim);
+    DIRS = DIRS.filter(vect => vect.some(v => v !== 0));
+
+    let res = false, totalBingos = 0;
+
+    for (let nr of hits) {
+        let coords = getHitCoords(nr);
+        //console.log('hit coords for', nr, coords);
+        // now we have the coords of a hit
+        if (coords === false) return true;
+        // let's mark it
+        setVal(coords, -1);
+
+        // now, for each directional vector (from DIRS), let's get to the edge of the object (needs to account correctly for dimension param)
+        // and then move forward 5 times and count values
+        // important: DIRs need to have a leading 0 added to them to account for whole cards array
+        let bingos = 0;
+
+        for (const vect of DIRS) {
+            let fullVect = [0, ...vect]; // including indexing the card
+
+            let fullVectBW = mulVect(fullVect, -1);
+
+            let tmpCoords = coords.slice(0);
+
+            while (vectInside( addVect(tmpCoords, fullVectBW) )) {
+                tmpCoords = addVect(tmpCoords, fullVectBW);
+            }
+
+            //console.log('processing direction', vect, '; search will be starting from coords', tmpCoords);
+
+            let marked = 0;
+            
+            while (vectInside( addVect(tmpCoords, fullVect) )) {
+                if (getVal(tmpCoords) === -1) marked++;
+                tmpCoords = addVect(tmpCoords, fullVect);
+            }
+            if (getVal(tmpCoords) === -1) marked++; // remaining one
+
+            if (marked === 5) bingos++;
+        }
+
+        totalBingos += Math.floor(bingos / 2); // each direction was processed twice
+
+        if (totalBingos >= 5) {
             res = nr;
             break;
         }
-
     }
+    
     return res;
 }
 
-console.log('p2', run(parse(input)))
-console.log('p3 tricked', run(parse(input), 1)) // removing the div accidentally hit the answer for p3, tbd verify if thats generic enough (likely not)
-// see script.p3.js for proper solution
+console.log('p1', run(parse(input), 2))
+console.log('p2', run(parse(input), 3))
+console.log('p3', run(parse(input), 4))
